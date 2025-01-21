d37f3fbb4e1e58b65dba931c240f640c40006c74
faizamairaj <faizamairaj0@gmail.com>
2025-01-21 10:45:58 +0500

    toggle-visibility-task-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,31 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 import React from 'react';
 
-
 function App() {
-   const [Visibility , setVisibility] = useState (true);
+  const [Visibility, setVisibility] = useState(true);
   return (
     <>
-     <h1 className='text-5xl text-white font-bold'>Toggle Visibility</h1>
-    <div  className='text-3xl font-bold rounded-md p-5 mt-72'>
-      <button className='bg-blue-400 text-white p-3 rounded' onClick={() =>{setVisibility(!Visibility)}}>Toggle</button>
-      {Visibility && <h3 className='text-pink-700'>A toggle button is a user interface element that allows users to switch between two states, such as on/off or show/hide.</h3>}
-    </div>
+      <h1 className="text-5xl text-white font-bold text-center sm:text-4xl md:text-5xl lg:text-6xl">
+        Toggle Visibility
+      </h1>
+      <div className="text-3xl font-bold rounded-md p-5 mt-20 sm:mt-10 md:mt-15 lg:mt-20 xl:mt-28">
+        <button
+          className="bg-blue-400 text-white p-3 rounded sm:text-sm md:text-base lg:text-lg xl:text-xl"
+          onClick={() => {
+            setVisibility(!Visibility);
+          }}
+        >
+          Toggle
+        </button>
+        {Visibility && (
+          <h3 className="text-pink-700 mt-5 sm:text-sm md:text-base lg:text-lg xl:text-xl">
+            A toggle button is a user interface element that allows users to switch between two states, such as on/off or show/hide.
+          </h3>
+        )}
+      </div>
     </>
   );
 }
+
 export default App;
-
-
